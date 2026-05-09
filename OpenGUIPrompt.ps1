ad883aa570ff53fdb693a0881bd371348bdce566
MiniKlar <omontloris@gmail.com>
2026-05-10 01:22:35 +0200

    - Added OpenGUIPrompt.ps1 script to prompt the user to retrieve information

---
+++ OpenGUIPrompt.ps1
@@ -0,0 +1,12 @@
+Param(
+[string]$promptString    
+)
+
+Add-Type -AssemblyName Microsoft.visualBasic
+
+$test = [Microsoft.visualBasic.Interaction]::InputBox($promptString, "AutomaticDiscovery")
+
+$test 
+
+#$script = $PSScriptRoot+"\OpenGUIPrompt.ps1" //chemin du script
+#& $script -promptString "Please enter your xxx" //lancer script
