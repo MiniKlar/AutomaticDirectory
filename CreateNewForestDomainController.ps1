9d2197694b0fc22ba3a9628bb17226d7ab78e868
MiniKlar <omontloris@gmail.com>
2026-05-13 00:20:28 +0200

    - Added a verification to ensure that an Active Directory is installed on the Windows Server

--- CreateNewForestDomainController.ps1
+++ CreateNewForestDomainController.ps1
@@ -12,6 +12,14 @@ Param(
 [string]$NetbiosName
 )
 
+try {
+    Import-Module ActiveDirectory -ErrorAction Stop
+}
+catch {
+    Write-Error "Failed to import ActiveDirectory module. Ensure you have the necessary permissions and the module is installed."
+    exit 1
+}
+
 $password = & $PSScriptRoot"\PasswordGUIPrompt.ps1"
     
 Install-ADDSForest -DomainName $DomainAddress -DomainNetbiosName $NetbiosName -SafeModeAdministratorPassword $password -Force
