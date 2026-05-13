1ecc38654d774693da6f2c2bc7c5550179a1ec39
unknown <Administrator@TEST.COM>
2026-05-13 03:33:15 -0700

    - Finished SaveDataBase script to save information about users and groups according to certain properties choosed by the user
    - Created LoadDataBase script to load the previously saved database created by our script

---
+++ LoadDataBase.ps1
@@ -0,0 +1,17 @@
+﻿<#.Name#>
+
+[CmdletBinding(PositionalBinding=$false)]
+
+Param(
+[Parameter(Mandatory=$true)]
+[ValidateNotNullOrEmpty()]
+[string]$PathToLoad,
+
+[Parameter(Mandatory=$true)]
+[ValidateNotNullOrEmpty()]
+[char]$FileDelimiter
+)
+
+$FileOutput = Import-Csv -Delimiter $FileDelimiter -Path $PathToLoad
+
+Write-Host $FileOutput
---
+++ SaveDataBase.ps1
@@ -0,0 +1,41 @@
+﻿<#.Name#>
+
+[CmdletBinding(PositionalBinding=$false)]
+
+Param(
+[Parameter(Mandatory=$true)]
+[ValidateNotNullOrEmpty()]
+[string]$PathToSave,
+
+[Parameter(Mandatory=$true)]
+[ValidateNotNullOrEmpty()]
+<<<<<<< Updated upstream
+[char]$DesiredDelimiter
+)
+#Auto error handeling if a char is not passed as a parameter
+
+Get-ADUser -Filter * | Export-Csv -Path $PathToSave -Delimiter $DesiredDelimiter
+
+"#TYPE Microsoft.ActiveDirectory.Management.ADGroup" | Add-Content -Path $PathToSave
+
+Get-ADGroup -Filter * | Export-Csv -Path $PathToSave -Delimiter $DesiredDelimiter -Append -Force
+=======
+[char]$DesiredDelimiter,
+
+[Parameter(Mandatory=$true)]
+[ValidateNotNullOrEmpty()]
+[String[]]$UserProperties,
+
+[Parameter(Mandatory=$true)]
+[ValidateNotNullOrEmpty()]
+[String[]]$GroupProperties
+)
+
+#Auto error handeling if a char is not passed as a parameter
+
+$Users = Get-ADUser -Filter * -Properties $UserProperties | Select-Object $UserProperties | Export-Csv -NoTypeInformation -Path $PathToSave -Delimiter $DesiredDelimiter
+
+#"#TYPE Microsoft.ActiveDirectory.Management.ADGroup" | Add-Content -Path $PathToSave
+
+$Groups = Get-ADGroup -Filter * -Properties $GroupProperties | Select-Object $GroupProperties | Export-Csv -NoTypeInformation -Path $PathToSave -Delimiter $DesiredDelimiter -Append -Force
+>>>>>>> Stashed changes
