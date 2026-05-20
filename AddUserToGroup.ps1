67b5ec463f90fab2cceccf04858c0f9e717ebba7
unknown <Administrator@test.com>
2026-05-19 19:58:57 -0700

    - Added user's variable verification to avoid that the script crashs if the group does not exist or if the user is already in it.

--- AddUserToGroup.ps1
+++ AddUserToGroup.ps1
@@ -19,5 +19,23 @@ try {
     exit 1
 }
 
+try {
+    Get-ADGroup -Identity $GroupName | Out-Null
+} catch {
+    Write-Host "Group does not exist."
+    exit 1
+}
+
+try {
+    $var = Get-ADGroupMember -Identity $GroupName | Select-Object -Property "SamAccountName"
+    if ($var.SamAccountName -eq "$UserName") {
+       throw
+    }
+} catch {
+    Write-Host "User is already in $GroupName group"
+    exit 1
+}
+
 Add-ADGroupMember -Identity $GroupName -Members $UserName
 Write-Host "$UserName has been added to $GroupName!"
+exit 0
